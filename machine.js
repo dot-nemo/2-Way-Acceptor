fbe91138ecc8437c73edca4f915a0aeb724a7441
stanpai78 <stanpai78@gmail.com>
2023-07-24 14:27:46 +0800

    Can read input backwards

--- machine.js
+++ machine.js
@@ -1,16 +1,18 @@
 document.addEventListener("DOMContentLoaded", e => {
     const q = []
     const F = []
-
-    let stepwatch = true
+    let lEnd = '⋊', rEnd = '⋉';
+    const right = true, left = false;
+    let lTouch = false, rTouch = false;
 
     function State(name) {
         this.name = name;
         this.δ = [];
     }
 
-    function Transition(read, destination) {
+    function Transition(read, direction, destination) {
         this.read = read;
+        this.direction = direction
         this.destination = destination;
     }
 
@@ -26,15 +28,22 @@ document.addEventListener("DOMContentLoaded", e => {
             const timelines = []
             for (let i = 0; i < this.q_i.δ.length; i++) {
                 const δ = this.q_i.δ[i];
-                if (δ.read == '') {
-                    const path = `(λ) => ${δ.destination.name}`;
+                if (δ.read == this.ω[this.head]) {
+                    const path = `(${δ.read}) ; ${δ.direction ? '+' : '-'} => ${δ.destination.name}`;
+                    if (log) console.log('Found %s', path);
+                    timelines.push(new Timeline(δ.destination, this.ω, δ.direction ? this.head + 1 : this.head - 1, this.path + path));
+                    if (δ.read == lEnd) {
+                        lTouch = true;
+                    } else if (δ.read == rEnd) {
+                        rTouch = true;
+                    }
+                } else if (δ.read == '') {
+                    const path = `(λ) ; ${this.direction ? '+' : '-'} => ${δ.destination.name}`;
                     if (log) console.log('Lambda transition found: %s', path);
-                    if (!(this.head == this.ω.length && F.includes(this.q_i)))
+                    if (!(lTouch && rTouch && F.includes(this.q_i) && this.q_i.δ.length == 0))
                         timelines.push(new Timeline(δ.destination, this.ω, this.head, this.path + path));
-                } else if (δ.read == this.ω[this.head]) {
-                    const path = `(${δ.read}) => ${δ.destination.name}`;
-                    if (log) console.log('Accepted %s', path);
-                    timelines.push(new Timeline(δ.destination, this.ω, this.head + 1, this.path + path));
+                    else
+                        console.log(`but ${this.q_i.name} is a final state with nothing left to read`);
                 }
             }
             return timelines;
@@ -44,81 +53,73 @@ document.addEventListener("DOMContentLoaded", e => {
     const input = document.getElementById('input');
     const run = document.getElementById('run');
     const step = document.getElementById('step');
+    const reset = document.getElementById('reset');
     run.onclick = async function() {
+        reset.onclick();
         const indisp = document.getElementById('input_display');
-        const accept = await fulltest(input.value, q[0], true, 500);
+        const accept = await fulltest(lEnd + input.value + rEnd, q[0], true, 250, 0);
         if (accept) {
-            indisp.style.backgroundColor = ''
+            indisp.style.backgroundColor = 'hsl(127, 100%, 90%)'
+        } else {
+            indisp.style.backgroundColor = 'hsl(0, 100%, 90%)'
         }
         console.log(
             (input.value.length == 0 ? 'λ' : input.value) + ' is ' + (accept ? 'accepted' : 'rejected')
             );
     }
 
+    let stepCtr = 1;
     step.onclick = async function() {
+        const indisp = document.getElementById('input_display');
+        const accept = await fulltest(lEnd + input.value + rEnd, q[0], true, 250, stepCtr);
 
+        if (accept != null) {
+            if (accept) {
+                indisp.style.backgroundColor = 'hsl(127, 100%, 90%)'
+            } else {
+                indisp.style.backgroundColor = 'hsl(0, 100%, 90%)'
+            }
+            console.log(
+                (input.value.length == 0 ? 'λ' : input.value) + ' is ' + (accept ? 'accepted' : 'rejected')
+                );
+
+        }
+            stepCtr++;
+    }
+
+    reset.onclick = function() {
+        stepCtr = 1;
+        resetDisplayBG();
     }
 
     q.push(
-        new State('q0*'),
+        new State('q0'),
         new State('q1'),
         new State('q2'),
         new State('q3'),
-        new State('q4*'),
-        new State('q5'),
-        new State('q6'),
-        new State('q7*'),
-        new State('q8'),
-        new State('q9')
+        new State('q4*')
     );
 
-    F.push(q[0], q[2]);
+    F.push(q[4]);
 
     q[0].δ.push(
-        new Transition('0', q[0]),
-        new Transition('', q[1]),
-        new Transition('', q[5])
+        new Transition(lEnd, right, q[0]),
+        new Transition('A', right, q[0]),
+        new Transition('B', right, q[0]),
+        new Transition(rEnd, left, q[1])
     );
 
     q[1].δ.push(
-        new Transition('0', q[2])
+        new Transition('A', left, q[2]),
+        new Transition('B', left, q[2])
     );
 
     q[2].δ.push(
-        new Transition('1', q[3])
+        new Transition('A', left, q[4]),
+        new Transition('B', left, q[3])
     );
 
-    q[3].δ.push(
-        new Transition('0', q[4])
-    );
-
-    q[4].δ.push(
-        new Transition('', q[8]),
-        new Transition('1', q[3])
-    );
-
-    q[5].δ.push(
-        new Transition('1', q[6])
-    );
-
-    q[6].δ.push(
-        new Transition('1', q[7])
-    );
-
-    q[7].δ.push(
-        new Transition('', q[8]),
-        new Transition('1', q[7])
-    );
-
-    q[8].δ.push(
-        new Transition('', q[9])
-    );
-
-    q[9].δ.push(
-        new Transition('', q[0])
-    );
-
-    async function fulltest(ω, q_i, log, delay) {
+    async function fulltest(ω, q_i, log, delay, step) {
         const display = document.getElementById('input_display');
         display.innerHTML = '';
         for (let i = 0; i < ω.length; i++) {
@@ -126,18 +127,20 @@ document.addEventListener("DOMContentLoaded", e => {
             display.innerHTML += `<span id=disp_${i} style>${c}</span>`;
         }
         resetDisplayBG();
-        document.getElementById('disp_0').style.backgroundColor = '	hsl(0, 100%, 90%)';
+        if (ω.length == 0) {
+            display.innerHTML = `<span id=disp_0 style>λ</span>`;
+        }
 
-        if (log) console.log('Testing: ' + ω + ' on ' + q_i.name);
+        if (log) console.log('Testing: ' + (ω.length > 0 ? ω : 'λ') + ' on ' + q_i.name);
         let tls = [new Timeline(q_i, ω, 0, '>' + q_i.name)];
         let stepCtr = 1;
         while(true) {
-            await sleepNow(delay)
+            if (step == 0) await sleepNow(delay)
             if (log) console.log('Step %d', stepCtr);
             const newTLs = [];
             let tlCtr = 0;
             tls.forEach(tl => {
-                if (log) console.log('Timeline %d', tlCtr);
+                if (log) console.log('Timeline %d starts on %s; head on %d; reading %s', tlCtr, tl.q_i.name, tl.head, (ω.length > 0 ? tl.ω[tl.head] : 'λ') );
                 let found = false;
                 tl.step(log).forEach(tl2 => {
                     found = true;
@@ -150,38 +153,47 @@ document.addEventListener("DOMContentLoaded", e => {
             });
             stepCtr++;
 
+            const multi = 256 / tls.length;
+            resetDisplayBG();
+            for (let i = tls.length - 1; i >= 0; i--) {
+                const tl = tls[i];
+                const color = Math.round(multi*i);
+                if (tl.head < ω.length) {
+                    const disp = document.getElementById(`disp_${tl.head}`)
+                    if (step == 0) await sleepNow(50)
+                    disp.style.backgroundColor = `hsl(${color%256}, 100%, 90%)`;
+                }
+            }
+
             if (newTLs.length == 0) {
+                step = 0
                 break;
             } else {
                 tls = newTLs;
-                const multi = 256 / tls.length;
-
-                resetDisplayBG();
-                for (let i = 0; i < tls.length; i++) {
-                    const tl = tls[i];
-                    const color = Math.round(multi*i);
-                    if (tl.head < ω.length) {
-                        document.getElementById(`disp_${tl.head}`).style.backgroundColor = `hsl(${color%256}, 100%, 90%)`;
-                        await sleepNow(100)
-                        // console.log(tl.head);
-                    }
-                }
             }
+            if (stepCtr == step + 1)
+                break;
         }
         let accept = false
-        for (let i = 0; i < tls.length; i++) {
+        for (let i = 0; i < tls.length && step == 0; i++) {
             const tl = tls[i];
-            if (F.includes(tl.q_i) && tl.head == ω.length) {
+            if (F.includes(tl.q_i)) {
+                resetDisplayBG();
                 accept = true;
                 console.log(tl.path);
                 break;
             }
+        }
+        if (step != 0 && tls.length != 0) {
+            return null;
         }
         return accept;
     }
 });
 
 function resetDisplayBG() {
+    const indisp = document.getElementById('input_display');
+    indisp.style.backgroundColor = '';
     const displays = document.querySelectorAll('#input_display > span');
     displays.forEach(disp => {
         disp.style.backgroundColor = '';
